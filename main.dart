981b204ad749875ddb81184cf05219d71e3e6c46
BluePyTheDeer_ <bluepythewolfdeer@proton.me>
2024-10-12 12:10:01 -0600

    Some learning done

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
 //Why void and not int? idk ask the syntax
 void main() {
   print("Hello, this already looks like Python mixed with C");
+  var randomNumber = 251;
+  var randomWord = "nerf";
+  print(randomNumber);
+  print(randomWord);
 }
