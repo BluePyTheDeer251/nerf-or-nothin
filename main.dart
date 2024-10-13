e6d0ac77a0f8561f64e02343216f6509bc441862
Paul <bluepythewolfdeer@proton.me>
2024-10-12 19:43:51 -0600

    Experiments

--- main.dart
+++ main.dart
@@ -1,10 +1,19 @@
 //Why void and not int? idk ask the syntax
 void main() {
   print("Hello, this already looks like Python mixed with C");
+  
   var randomNumber = 251;
   var randomWord = "nerf";
   var randomArray = ["One", "Two", "Three"];
+  
   print(randomNumber);
   print(randomWord);
   print(randomArray);
+
+  if (randomNumber >= 125) {
+    print("More than 125");
+  }
+  else {
+    print("Less than 125");
+  }
 }
