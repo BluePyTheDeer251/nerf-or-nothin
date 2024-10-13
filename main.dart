5c4a925266e5da53ce6f6f8248e827b1a973268e
Paul <bluepythewolfdeer@proton.me>
2024-10-12 19:35:58 -0600

    Experiments

--- main.dart
+++ main.dart
@@ -3,6 +3,8 @@ void main() {
   print("Hello, this already looks like Python mixed with C");
   var randomNumber = 251;
   var randomWord = "nerf";
+  var randomArray = ["One", "Two", "Three"];
   print(randomNumber);
   print(randomWord);
+  print(randomArray);
 }
